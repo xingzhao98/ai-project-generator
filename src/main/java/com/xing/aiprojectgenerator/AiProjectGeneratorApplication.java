75de1bcf726a8bfc29ecc7c91d0759245d316a06
ZhaoXing <lalaeric@qq.com>
2025-08-29 13:47:25 +0800

    init

---
+++ src/main/java/com/xing/aiprojectgenerator/AiProjectGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.xing.aiprojectgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProjectGeneratorApplication.class, args);
+    }
+
+}
