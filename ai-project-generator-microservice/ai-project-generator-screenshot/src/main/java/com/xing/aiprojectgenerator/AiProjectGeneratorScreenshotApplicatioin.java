714a5ff4d939fc616ae8871dd98ac202c64dfc7d
ZhaoXing <lalaeric@qq.com>
2025-09-12 23:02:20 +0800

    微服务改造 - 截图服务（代码移动） 顺便修改application-local.yml

---
+++ ai-project-generator-microservice/ai-project-generator-screenshot/src/main/java/com/xing/aiprojectgenerator/AiProjectGeneratorScreenshotApplicatioin.java
@@ -0,0 +1,11 @@
+package com.xing.aiprojectgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjectGeneratorScreenshotApplicatioin {
+    public static void main(String[] args) {
+        SpringApplication.run(AiProjectGeneratorScreenshotApplicatioin.class, args);
+    }
+}
